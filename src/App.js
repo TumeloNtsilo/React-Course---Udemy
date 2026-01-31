f9d55513a601c7c31d65202c67da3c76dc24a79c
Tumelo Ntsilo <tumelontsilo@gmail.com>
2026-01-31 16:23:01 +0200

    Initial commit - added React practice project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+export default function App(){
+  return <div className="steps">
+            <div className="numbers">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+
+            <div className="message"> 
+              <p>Hello Tumelo</p>
+            </div>
+
+            <div className="buttons">
+              <button>Previous</button>
+              <button>Next</button>
+            </div>
+          </div>
+}
